a373dbfde9387bc5849492f8d80a2c6d44b8016f
maeda yoshiya <hikowaiide@gmail.com>
2016-02-22 17:51:34 +0900

    yoshiya_test

--- app/src/main/java/com/example/xjapan/karaoke2/MainActivity.java
+++ app/src/main/java/com/example/xjapan/karaoke2/MainActivity.java
@@ -5,12 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     //test1
+    //testよしやだよー
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
